304e0afade2d16544c704745efa107845d56f3ac
Marin Cabac <marin.cabac@gmail.com>
2018-03-17 14:06:03 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,7 +3,7 @@ class Person
   :weight, :handed, :complexion, :t_shirt_size,
   :wrist_size, :glove_size, :pant_length, :pant_width
   def initialize(hash)
-    hash.each {|k,v| self.send("#{{k}=}",v)}
+    hash.each {|k,v| self.send("#{k}=",v)}
 
   end
 end
